c179af0d67fb8b160e8365d89bf50fe959c81685
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-06-28 09:37:08 +0530

    First Version Of One Place Shell APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { IonApp, IonRouterOutlet, IonToolbar, IonContent, IonItem, IonList, IonHeader, IonTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+   imports: [IonTitle, IonHeader, IonList, IonItem, IonContent, IonToolbar, IonApp, IonRouterOutlet,RouterLink,RouterOutlet],
+
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+   {
+    path: 'music-player',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Routes',
+      }).then(m => m.routes),
   },
 ];
